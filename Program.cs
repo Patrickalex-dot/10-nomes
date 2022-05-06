b754b8c5525c771be2b04661a54e75d39ce1ecfd
patrick alexandre <palexandre@live.com>
2022-05-05 21:56:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_nomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+            for(int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("insira um nome");
+                nomes[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"os nomes da sua lista são: {nomes[i]} ");
+            }
+            
+
+
+
+
+        }
+    }
+}
